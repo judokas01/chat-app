852a4d5087d69e711dd100ced9b231776cb821d7
Šimon Zajíček <simon.zajicek@qest.cz>
2024-06-08 20:29:43 +0200

    prisma schema

--- apps/chat-app/src/app/app.module.ts
+++ apps/chat-app/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common'
 
+import { PrismaService } from '../infrastructure/prisma/prisma.service'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
     controllers: [AppController],
     imports: [],
-    providers: [AppService],
+    providers: [AppService, PrismaService],
 })
 export class AppModule {}
