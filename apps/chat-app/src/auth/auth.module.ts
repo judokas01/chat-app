3f815303951fcff7a8439284dcef7311c82258ff
Šimon Zajíček <simon.zajicek@qest.cz>
2024-06-22 16:41:58 +0200

    added wip controllers

--- apps/chat-app/src/auth/auth.module.ts
+++ apps/chat-app/src/auth/auth.module.ts
@@ -4,8 +4,11 @@ import { LoginService } from './login/login.service'
 import { RegisterService } from './register/register.service'
 import { JwtAuthenticateService } from './authenticate/services/jwt-authenticate.service'
 import { JWT } from './common/jwt.module'
+import { RestController } from './controllers/rest/rest.controller'
+import { GqlController } from './controllers/gql/gql.controller'
 
 @Module({
+    controllers: [RestController, GqlController],
     imports: [JWT],
     providers: [LoginService, RegisterService, JwtAuthenticateService, ConfigService],
 })
---
+++ apps/chat-app/src/auth/controllers/gql/gql.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common'
+
+@Controller('gql')
+export class GqlController {}
